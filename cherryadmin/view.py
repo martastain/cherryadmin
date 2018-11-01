6a30834da729f6832aba2843bb659de3ded2eb58
Martastain <martas@imm.cz>
2018-11-01 14:53:55 +0100

    views clean-up

--- cherryadmin/view.py
+++ cherryadmin/view.py
@@ -1,30 +1,3 @@
-class CherryAdminView(object):
-    is_raw = False
-
-    def __init__(self, name, context, *args, **kwargs):
-        self.context = context
-        self.view = name
-        self.context["response"] = 200
-        self["name"] = name
-        self["mime"] = "text/html"
-
-    @property
-    def name(self):
-        return self.context["page"]["name"]
-
-    def __getitem__(self, key):
-        return self.context[key]
-
-    def __setitem__(self, key, value):
-        self.context["page"][key] = value
-
-    def auth(self):
-        return self["user"]
-
-    def build(self, *args, **kwargs):
-        self.context["page"].update(kwargs)
-
-
 class CherryAdminRawView(object):
     is_raw = True
 
@@ -51,3 +24,7 @@ class CherryAdminRawView(object):
 
     def build(self, *args, **kwargs):
         self.context["page"].update(kwargs)
+
+
+class CherryAdminView(CherryAdminRawView):
+    is_raw = False
